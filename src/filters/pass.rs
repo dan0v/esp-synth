208af1875de672570af4eeeb693efa2a89abb4f1
Lukas Engelke <lukas.engelke@tngtech.com>
2024-11-20 21:59:35 +0000

    Refactor BiquadLowPass to use generic DF2Filter, add BiquadHighPassFilter

--- src/filters/pass.rs
+++ src/filters/pass.rs
@@ -5,111 +5,157 @@ use crate::{
 };
 use core::f32::consts::TAU;
 
-// https://www.dsprelated.com/freebooks/filters/Biquad_Software_Implementations.html
+/// Generic filter in Direct Form II
+///
+/// Generic parameter is Order + 1
+pub struct DF2Filter<const N: usize> {
+    a: [f32; N],
+    b: [f32; N],
+    v: [f32; N],
+}
+
+impl<const N: usize> DF2Filter<N> {
+    pub const ORDER: usize = N - 1;
+
+    pub fn new(a: [f32; N], b: [f32; N]) -> Self {
+        DF2Filter { a, b, v: [0.; N] }
+    }
+
+    pub fn set_coefficients(&mut self, a: [f32; N], b: [f32; N]) {
+        self.b = b.map(|x| x / a[0]);
+        self.a = a.map(|x| x / a[0]);
+    }
+}
+
+impl<const N: usize> Filter for DF2Filter<N> {
+    type In = f32;
+    type Out = f32;
+
+    fn filter(&mut self, x: Self::In) -> Self::Out {
+        let mut v0 = self.b[0] * x;
+        for i in 1..N {
+            v0 -= self.a[i] * self.v[i];
+            self.v[i] = self.v[i - 1];
+        }
+        self.v[0] = v0;
+
+        let mut y = v0;
+        for i in 1..N {
+            y += self.b[i] * self.v[i];
+        }
+        y
+    }
+}
+
+// https://pytorch.org/audio/main/_modules/torchaudio/functional/filtering.html#lowpass_biquad
 pub struct BiquadLowPassFilter {
+    df2: DF2Filter<3>,
     cutoff_freq: f32,
     q: f32,
-    b0: f32,
-    b1: f32,
-    b2: f32,
-    a1: f32,
-    a2: f32,
-    z1: f32,
-    z2: f32,
 }
 
 impl BiquadLowPassFilter {
     pub fn new() -> Self {
-        let mut flt = BiquadLowPassFilter {
-            cutoff_freq: REFERENCE_FREQ,
-            q: 0.72,
-            b0: 0.0,
-            b1: 0.0,
-            b2: 0.0,
-            a1: 0.0,
-            a2: 0.0,
-            z1: 0.0,
-            z2: 0.0,
-        };
-        flt.update_coefficients();
-        flt
+        let cutoff_freq = REFERENCE_FREQ;
+        let q = 0.72;
+        let (a, b) = Self::get_coefficients(cutoff_freq, q);
+        BiquadLowPassFilter {
+            df2: DF2Filter::new(a, b),
+            cutoff_freq,
+            q,
+        }
     }
 
     pub fn set_cutoff(&mut self, cutoff_freq: f32) {
         self.cutoff_freq = cutoff_freq;
-        self.update_coefficients();
+        let (a, b) = Self::get_coefficients(self.cutoff_freq, self.q);
+        self.df2.set_coefficients(a, b);
     }
 
     pub fn set_q(&mut self, q: f32) {
         self.q = q;
-        self.update_coefficients();
+        let (a, b) = Self::get_coefficients(self.cutoff_freq, self.q);
+        self.df2.set_coefficients(a, b);
     }
 
-    fn update_coefficients(&mut self) {
-        let omega = TAU * self.cutoff_freq * DT;
-        let alpha = sin(omega) / (2.0 * self.q);
+    fn get_coefficients(cutoff_freq: f32, q: f32) -> ([f32; 3], [f32; 3]) {
+        let omega = TAU * cutoff_freq * DT;
+        let alpha = sin(omega) / (2.0 * q);
         let omega_cos = cos(omega);
 
-        let a0 = 1.0 + alpha;
-        let a1 = -2.0 * omega_cos;
-        let a2 = 1.0 - alpha;
-
-        let b1 = 1.0 - omega_cos;
-        let b0 = 0.5 * b1;
-        let b2 = b0;
-
-        self.b0 = b0 / a0;
-        self.b1 = b1 / a0;
-        self.b2 = b2 / a0;
-        self.a1 = a1 / a0;
-        self.a2 = a2 / a0;
+        (
+            [1.0 + alpha, -2.0 * omega_cos, 1.0 - alpha],
+            [
+                0.5 - 0.5 * omega_cos,
+                1.0 - omega_cos,
+                0.5 - 0.5 * omega_cos,
+            ],
+        )
     }
 }
 
 impl Filter for BiquadLowPassFilter {
     type In = f32;
     type Out = f32;
-    fn filter(&mut self, x: f32) -> f32 {
-        let y = self.b0 * x - self.a1 * self.z1 - self.a2 * self.z2;
-        self.z2 = self.z1;
-        self.z1 = y;
 
-        y + self.b1 * self.z1 + self.b2 * self.z2
+    fn filter(&mut self, x: Self::In) -> Self::Out {
+        self.df2.filter(x)
     }
 }
 
-pub struct SimpleLowPassFilter {
-    z1: f32, // system state from one time step previously
-    a: f32,
-    b: f32,
+// https://pytorch.org/audio/main/_modules/torchaudio/functional/filtering.html#highpass_biquad
+pub struct BiquadHighPassFilter {
+    df2: DF2Filter<3>,
+    cutoff_freq: f32,
+    q: f32,
 }
 
-impl SimpleLowPassFilter {
-    pub fn new(alpha: f32) -> Self {
-        SimpleLowPassFilter {
-            z1: 0.0,
-            a: 1.0 - alpha,
-            b: alpha,
+impl BiquadHighPassFilter {
+    pub fn new() -> Self {
+        let cutoff_freq = REFERENCE_FREQ;
+        let q = 0.72;
+        let (a, b) = Self::get_coefficients(cutoff_freq, q);
+        BiquadHighPassFilter {
+            df2: DF2Filter::new(a, b),
+            cutoff_freq,
+            q,
         }
     }
 
-    pub fn set_alpha(&mut self, alpha: f32) {
-        // let omega = TAU * cutoff_freq * DT;
-        // let alpha = sin(omega);
+    pub fn set_cutoff(&mut self, cutoff_freq: f32) {
+        self.cutoff_freq = cutoff_freq;
+        let (a, b) = Self::get_coefficients(self.cutoff_freq, self.q);
+        self.df2.set_coefficients(a, b);
+    }
 
-        self.b = alpha;
-        self.a = 1. - alpha;
+    pub fn set_q(&mut self, q: f32) {
+        self.q = q;
+        let (a, b) = Self::get_coefficients(self.cutoff_freq, self.q);
+        self.df2.set_coefficients(a, b);
+    }
+
+    fn get_coefficients(cutoff_freq: f32, q: f32) -> ([f32; 3], [f32; 3]) {
+        let omega = TAU * cutoff_freq * DT;
+        let alpha = sin(omega) / (2.0 * q);
+        let omega_cos = cos(omega);
+
+        (
+            [1.0 + alpha, -2.0 * omega_cos, 1.0 - alpha],
+            [
+                0.5 + 0.5 * omega_cos,
+                -1.0 - omega_cos,
+                0.5 + 0.5 * omega_cos,
+            ],
+        )
     }
 }
 
-impl Filter for SimpleLowPassFilter {
+impl Filter for BiquadHighPassFilter {
     type In = f32;
     type Out = f32;
 
-    fn filter(&mut self, x: f32) -> f32 {
-        let y = self.b * x - self.a * self.z1;
-        self.z1 = y;
-
-        y
+    fn filter(&mut self, x: Self::In) -> Self::Out {
+        self.df2.filter(x)
     }
 }
+
